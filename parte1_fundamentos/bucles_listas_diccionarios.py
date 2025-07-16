2ab62fc4d57a223a311e7f51509cf3857428ffb0
jorgene123 <160285821+jorgene123@users.noreply.github.com>
2025-07-15 18:00:39 -0600

    Create bucles_listas_diccionarios.py
    En Python, las listas, los diccionarios y los bucles son estructuras fundamentales para organizar y manipular datos. Las listas son secuencias ordenadas de elementos que pueden ser modificadas, mientras que los diccionarios son conjuntos no ordenados de pares clave-valor que también son mutables. Los bucles, por otro lado, permiten repetir un bloque de código varias veces, ya sea para iterar sobre una estructura de datos o hasta que se cumpla una condición. 
    
    Los bucles for se utilizan para iterar sobre una secuencia de elementos, como una lista o un diccionario, o sobre un rango de números. 
    Los bucles while se utilizan para repetir un bloque de código mientras se cumpla una condición específica. 
    Ambos tipos de bucles son esenciales para automatizar tareas repetitivas y procesar datos de manera eficiente.

---
+++ parte1_fundamentos/bucles_listas_diccionarios.py
@@ -0,0 +1,26 @@
+# bucles_listas_diccionarios.py
+
+#lista
+materias = ["biología", "procesamiento de bioseñales", "anatomía humana", "circuitos eléctricos"]
+materias.append("embebidos")
+materias.remove("procesamiento de bioseñales")
+
+print("Materias:", materias)
+
+#diccionario
+yo = {
+    "nombre": "Juanito",
+    "edad": 78,
+    "ciudad": "Tangamandapio",
+    "carrera": "Licenciado en videojuegos"
+}
+
+print(f"Carrera: {yo['carrera']}")
+yo["fav"] = "python"
+yo["ciudad"] = "Zapopan"
+
+print("Datos actualizados:", yo)
+
+#bucle for
+for i in range(1, 6):
+    print(f"Cuadrado de {i} es {i**2}")
